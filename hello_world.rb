585f431a2430ece9f09e7d2cbf6b5902803de651
Oguz Serbetci <oserbetci@babbel.com>
2015-12-23 13:26:02 +0100

    Roman_n

--- hello_world.rb
+++
@@ -1,5 +0,0 @@
-class HelloWorld
-  def self.hello(name = nil)
-    name.nil? ?  "Hello, World!" : "Hello, " + name + "!"
-  end
-end
--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,38 +1,62 @@
 class Integer
-	def to_roman
-		output = ''
-		numerals = ['M', 1000, 'D', 500, 'C', 100,
-								'L', 50, 	 'X', 10,  'V', 5,   'I', 1]
+# <<<<<<< Updated upstream
+# 	def to_roman
+# 		output = ''
+# 		numerals = ['M', 1000, 'D', 500, 'C', 100,
+# 								'L', 50, 	 'X', 10,  'V', 5,   'I', 1]
 
-		m = self
-		numerals.each_with_index do |element, index|
-			if index % 2 == 1
-				next
-			end
+# 		m = self
+# 		numerals.each_with_index do |element, index|
+# 			if index % 2 == 1
+# 				next
+# 			end
 
-			currentNumeral = numerals[index+1]
-			while m >= currentNumeral
-				q, m = m.divmod(currentNumeral)
-				if q == 4
-					output += element
-					output += numerals[index-2]
-				else
-					output += element * q
-				end
-			end
+# 			currentNumeral = numerals[index+1]
+# 			while m >= currentNumeral
+# 				q, m = m.divmod(currentNumeral)
+# 				if q == 4
+# 					output += element
+# 					output += numerals[index-2]
+# 				else
+# 					output += element * q
+# 				end
+# 			end
 
-			if numerals[index+3]
-				num = numerals[index+3]
-			else
-				num = 0
-			end
+# 			if numerals[index+3]
+# 				num = numerals[index+3]
+# 			else
+# 				num = 0
+# 			end
 
-			if m >= currentNumeral - num
-				m -= currentNumeral*0.9
-				output += numerals[index+4]
-				output += element
-			end
-		end
-		output
-	end
+# 			if m >= currentNumeral - num
+# 				m -= currentNumeral*0.9
+# 				output += numerals[index+4]
+# 				output += element
+# 			end
+# 		end
+# 		output
+# 	end
+# =======
+  def to_roman
+    romans = {1000 => 'M', 500 => 'D', 100 => 'C', 50 => 'L', 10 => 'X', 5 => 'V', 1 => 'I'}
+    string = ''
+
+    s = self
+
+    romans.each do |key, label|
+      while s - key >= 0 do
+        romans.each do |(key2, label2), index|
+          if s - key == key2
+            string << label2
+            s -= key
+          end
+        end
+        string << label
+        s -= key
+      end
+    end
+
+    return string
+  end
+# >>>>>>> Stashed changes
 end
